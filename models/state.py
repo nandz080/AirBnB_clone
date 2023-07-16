fe44140e7e2679fc0567677875700220a74e2a0e
nandz080 <nandz080@gmail.com>
2023-07-16 08:24:07 +0000

    updated state class

---
+++ models/state.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+""" Module defining State class
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Class representing a State that is inheriting from BaseModel """
+
+    name = ""
