7d00ed7f2922cbb1a9d2cb7c06d533540eb6b3e6
nandz080 <nandz080@gmail.com>
2023-07-16 21:01:53 +0000

    write testcase for user module

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" """
+from tests.test_models.test_base_model import test_basemodel
+from models.user import User
+import os
+
+
+class test_User(test_basemodel):
+    """ Testcase for testing User model"""
+
+    def __init__(self, *args, **kwargs):
+        """ user test class testing initialization of user class"""
+        super().__init__(*args, **kwargs)
+        self.name = "User"
+        self.value = User
+
+    def test_first_name(self):
+        """ first name attribute testing"""
+        new = self.value()
+        self.assertEqual(type(new.first_name), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
+
+    def test_last_name(self):
+        """ last name attribute testing"""
+        new = self.value()
+        self.assertEqual(type(new.last_name), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
+
+    def test_email(self):
+        """ email attribute testing"""
+        new = self.value()
+        self.assertEqual(type(new.email), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
+
+    def test_password(self):
+        """ password attribute testing"""
+        new = self.value()
+        self.assertEqual(type(new.password), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
