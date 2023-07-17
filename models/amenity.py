8626d06d7a887d9817c7806177a34d919c3f0539
Anele Mbatha <collenmbatha40@gmail.com>
2023-07-16 17:51:08 -0700

    Added more test cases and fixed pycodestyle errors

--- models/amenity.py
+++ models/amenity.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-
 """
 Module defining Amenity class
 """
+from models.base_model import BaseModel
+
 
 class Amenity(BaseModel):
-        """Represent an amenity.
+    """Represent an amenity.
     Attributes:
         name (str): The name of the amenity.
     """
 
     name = ""
+
--- models/review.py
+++ models/review.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-"""
-   Module defining reiviews class
+"""Module defining reiviews class
 """
 from models.base_model import BaseModel
 
 class Review(BaseModel):
-"""Class representing review that inherits from BaseModel"""
+    """Class representing review that inherits from BaseModel
+     Attributes:
+        place_id (str): The Place id.
+        user_id (str): The User id.
+        text (str): The text of the review.
+    """
 
-	place_id = ""
-	user_id = ""
-	text = ""
+    place_id = ""
+    user_id = ""
+    text = ""
+
